6f32c9d7ca8d450dbf2c22a9d6f8d64d3ec1236f
Mahamat Ali <mahamatali0005@gmail.com>
2025-12-18 22:16:18 +0100

    LSP : implémentation avant refactoring

---
+++ LSP/Rectangle_Avant.java
@@ -0,0 +1,36 @@
+package ict301.solid.lsp;
+
+// Violation du principe LSP
+public class Rectangle_Avant {
+
+    protected int width;
+    protected int height;
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getArea() {
+        return width * height;
+    }
+}
+
+// Classe dérivée problématique
+class Square extends Rectangle_Avant {
+
+    @Override
+    public void setWidth(int width) {
+        this.width = width;
+        this.height = width;
+    }
+
+    @Override
+    public void setHeight(int height) {
+        this.width = height;
+        this.height = height;
+    }
+}
